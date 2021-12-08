9b545d9a4fef8c93579c6283af30ece89fd2266a
hai li <Create-lihai>
2021-12-08 14:57:38 +0800

    first files
    first files

---
+++ src/main/java/cn/star/test/springtest.java
@@ -0,0 +1,21 @@
+package cn.star.test;
+import cn.star.service.UsersService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试类〉
+ *
+ */
+public class springtest {
+    @Test
+    public void Test(){
+        //加载配置文件
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        //获取对象
+        UsersService us = (UsersService) ac.getBean("usersService");
+        //调用方法
+        us.findUsers();
+    }
+}
